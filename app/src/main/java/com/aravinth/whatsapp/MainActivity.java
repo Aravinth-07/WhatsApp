bdb151d78f475eb1c55bcdd672e157e3d18d1377
Aravinth-07 <strong7aravinth2000@gmail.com>
2021-02-26 18:32:15 +0530

    Recyclerview is done with elements and FAB is added

--- app/src/main/java/com/aravinth/whatsapp/MainActivity.java
+++ app/src/main/java/com/aravinth/whatsapp/MainActivity.java
@@ -9,11 +9,21 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar custom_toolbar;
     private RecyclerView recyclerView;
+    String s1[],s2[];
+    int images[] = {R.drawable.mydp1,R.drawable.mydp1,R.drawable.mydp1,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,
+            R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,
+            R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,
+            R.drawable.mydp,R.drawable.mydp,R.drawable.mydp,R.drawable.mydp};
 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -22,10 +32,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        TabItem chats = findViewById(R.id.chats);
+        TabItem status = findViewById(R.id.status);
+        TabItem calls = findViewById(R.id.chats);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
         custom_toolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(custom_toolbar);
         getSupportActionBar().setTitle("WhatsApp");
 
+        recyclerView = findViewById(R.id.recycler_view);
+        s1 = getResources().getStringArray(R.array.name);
+        s2 = getResources().getStringArray(R.array.description);
+        customAdapter cust = new customAdapter(this,s1,s2,images);
+        recyclerView.setAdapter(cust);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         }
 
--- app/src/main/java/com/aravinth/whatsapp/customAdapter.java
+++ app/src/main/java/com/aravinth/whatsapp/customAdapter.java
@@ -1,8 +1,10 @@
 package com.aravinth.whatsapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,25 +12,48 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class customAdapter extends RecyclerView.Adapter<customAdapter.myviewHolder> {
 
+    String data1[],data2[];
+    int images[];
+    Context context;
+
+    public customAdapter(Context ct, String s1[], String s2[], int img[])
+    {
+       context = ct;
+       data1 = s1;
+       data2 = s2;
+       images = img;
+    }
+
     @NonNull
     @Override
     public myviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_model,parent,false);
+        return new myviewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull myviewHolder holder, int position) {
+        holder.name.setText(data1[position]);
+        holder.desc.setText(data2[position]);
+        holder.dp.setImageResource(images[position]);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data1.length;
     }
 
     public class myviewHolder extends RecyclerView.ViewHolder {
+
+        TextView name,desc;
+        ImageView dp;
         public myviewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.myname);
+            desc = itemView.findViewById(R.id.mydesc);
+            dp = itemView.findViewById(R.id.mydp);
         }
     }
 }
